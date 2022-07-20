59abd13e39766b04afd074d53108bbe2a1ae704f
n <nazmiahmad9@gmail.com>
2022-07-20 15:58:40 +0200

    silder with label

--- app/src/main/java/com/livelucky/lebenindeutschland/ui/components/CustomSlider.kt
+++ app/src/main/java/com/livelucky/lebenindeutschland/ui/components/CustomSlider.kt
@@ -1,8 +1,10 @@
 package com.livelucky.lebenindeutschland.ui.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Slider
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -10,14 +12,23 @@ import kotlin.math.roundToInt
 
 @Composable
 fun CustomSlider(onValueChangeFinished: (value: Int) -> Unit) {
-    var sliderPosition by remember { mutableStateOf(0f) }
-    Slider(
-        modifier = Modifier
-            .padding(10.dp)
-            .fillMaxWidth(),
-        value = sliderPosition, onValueChange = { sliderPosition = it },
-        valueRange = 1f..50f,
-        onValueChangeFinished = { onValueChangeFinished(sliderPosition.roundToInt()) }
-    )
+    var sliderPosition by remember { mutableStateOf(1f) }
+    Column(modifier = Modifier
+        .padding(10.dp)
+        .fillMaxWidth()) {
+        Text(text = "Number Of Questions in the Quiz : "+sliderPosition.roundToInt().toString())
+        Slider(
+            modifier = Modifier
+                .padding(5.dp)
+                .fillMaxWidth(),
+            value = sliderPosition, onValueChange = { sliderPosition = it },
+            valueRange = 1f..50f,
+            onValueChangeFinished = { onValueChangeFinished(sliderPosition.roundToInt()) },
+            steps = 50
+        )
+    }
+
+
+
 
 }

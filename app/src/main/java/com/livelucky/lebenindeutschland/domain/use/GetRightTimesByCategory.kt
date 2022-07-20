e273b24227b9bc3bb2364c38f5b86d346011c766
n <nazmiahmad9@gmail.com>
2022-07-20 16:03:15 +0200

    invocation classes

--- app/src/main/java/com/livelucky/lebenindeutschland/domain/use/GetRightTimesByCategory.kt
+++ app/src/main/java/com/livelucky/lebenindeutschland/domain/use/GetRightTimesByCategory.kt
@@ -1,2 +1,14 @@
-package com.livelucky.lebenindeutschland.domain.use 
+package com.livelucky.lebenindeutschland.domain.use
 
+import com.livelucky.lebenindeutschland.domain.model.Category
+import com.livelucky.lebenindeutschland.domain.model.Question
+import com.livelucky.lebenindeutschland.domain.repo.QuestionRepo
+
+class GetRightTimesByCategory (
+    private val repository: QuestionRepo
+) {
+    suspend operator fun invoke(category: Category): Int {
+        return repository.getRightTimesByCategory(category)
+    }
+
+}
